f89fef8a077d0ff958bbd53dca482efd25726c67
Houn <m18683659759@163.com>
2017-03-05 17:31:17 +0800

    js增加新功能，重写readme

--- README.md
+++ README.md
@@ -1,14 +1,86 @@
 #Stone
 ---
-Stone include stone.js and stone.scss/stone.css
-You can import these to init your project
+Stone 项目包括(JS/CSS)2个部分
 
 stone.js
-> include many often used function
+>这里收录了一些我常用的JS函数，以及算法学习中的算法
 
 stone.css
->include init css style and classname style,
->base on material design color
+>这里收集了我常用的css属性和搭配
 
-How to use
->direct import in your html/css/js
+使用
+>将你所需的文件直接引入你的项目即可
+
+>【详细可查看下面的说明文档】
+
+---
+stone.js
+
+1. 获取url中的指定值
+``` 
+S_getUrl(paramName) : 返回 查询当前url中你指定的参数paramName对应的值 
+```
+2. 有序表查找（顺序查找哨兵模式）
+```
+S_sentry(des, arr) : arr为有序数组，des是你要查找的目标 返回查找目标下标 
+```
+3. 有序表查找 （折半查找）
+```
+S_binarySearch(des, arr) : arr为有序数组，des是你要查找的目标 返回查找目标下标 
+```
+4. 冒泡排序(改进)
+```
+S_bubbleSort(arr) :arr 为数组，返回一个从小到大的有序数组
+```
+5. 简单选择排序
+```
+S_selectSort(arr) :arr 为数组，返回一个从小到大的有序数组
+```
+6. 快速排序
+```
+S_quickSort :arr 为数组，返回一个从小到大的有序数组
+```
+
+
+
+
+
+---
+
+stone.css
+
+1. margin/padding 强制为0
+``` 
+.m0 / .p0
+```
+2. margin-left/right/top/bottom | padding-left/right/top/bottom 强制为0
+```
+.ml0/.mr0/.mt0/.mb0 | .pl0/.pr0/.pt0/.pb0
+```
+3. 左/右浮动
+```
+.l/.r
+```
+4. 清除浮动
+```
+.cl
+```
+5. 文字超出省略
+```
+.omit
+```
+6. 文字居左/居右/居中
+```
+.tl/.tr/.tc
+```
+7. 按钮样式（基础）
+```
+.S_btn  : 简写=> .Sb
+```
+8. 按钮样式扩展 （要基于基础的按钮样式）
+```
+.S_btn_success  : 简写=> .Sbs  【用于关于成功】
+.S_btn_danger   : 简写=> .Sbd  【用于关于警告】
+.S_btn_info     : 简写=> .Sbi  【用于关于提示】
+
+```
--- stone.js
+++ stone.js
@@ -1,62 +1,156 @@
-//目前只支持单个
-
 //从url中获取某参数对应的值
 function S_getUrl(paramName) {
-    var url=location.search;
+    var url = location.search;
     //截取字符串，去掉第0个，使字符串从第一个字符开始
-    var str=url.substr(1);
-    var strArray=str.split("&");
-    for(var i in strArray){
-      var thisStrArray=strArray[i].split("=");
-      if(thisStrArray[0]==paramName){
-        return thisStrArray[1];
-      }
+    var str = url.substr(1);
+    var strArray = str.split("&");
+    for (var i in strArray) {
+        var thisStrArray = strArray[i].split("=");
+        if (thisStrArray[0] == paramName) {
+            return thisStrArray[1];
+        }
     }
-      return "paramName error"
+    return "paramName error"
 }
 
 //获取时间函数
-var S_time=new function(){
-  var myDate=new Date();
-  this.getSimpleDate=function(){
-    return myDate;
-  };
-  this.getWeekdat=function(){
-    return myDate.getDay();
-  }
-  this.getYear=function(){
-    return myDate.getFullYear();
-  }
-  this.getMonth=function(){
-    return myDate.getMonth()+1;
-  };
-  this.getDay=function(){
-    return myDate.getDate();
-  };
-  this.getHour=function(){
-    return myDate.getHours();
-  };
-  this.getMin=function(){
-    return myDate.getMinutes();
-  };
-  this.getSec=function(){
-    return myDate.getSeconds();
-  }
+var S_time = new function() {
+    var myDate = new Date();
+    this.getSimpleDate = function() {
+        return myDate;
+    };
+    this.getWeekdat = function() {
+        return myDate.getDay();
+    }
+    this.getYear = function() {
+        return myDate.getFullYear();
+    }
+    this.getMonth = function() {
+        return myDate.getMonth() + 1;
+    };
+    this.getDay = function() {
+        return myDate.getDate();
+    };
+    this.getHour = function() {
+        return myDate.getHours();
+    };
+    this.getMin = function() {
+        return myDate.getMinutes();
+    };
+    this.getSec = function() {
+        return myDate.getSeconds();
+    }
 }
-//ajax for ie9+
-// var ajax=function(method,url,data,success,fail){
-//   var request=new XMLHttpRequest();
-//   request.open(method,url,true);
-//   request.onload = function(){
-//     if(request.status>=200 && request.status<400){
-//       var res=request.responseText;
-//       success(res);
-//     }else{
-//       // We reached our target server, but it returned an error
-//     }
-//     request.onerror=function(){
-//       fail;
-//     }
-//   }
-//   request.send(data);
-// }
+
+//哨兵模式 （有序表查找）
+/**
+*查找的时候如果用for的，可以改进为whie(a!=des) 这样可以减少a的越界判断
+*/
+function S_sentry(des, arr) {
+    if (!(arr instanceof Array))
+        return "arr is not array";
+    var i;
+    arr[0] = des;
+    i = arr.length;
+    while (arr[i] != des) {
+        i--;
+    }
+    return i;
+}
+
+//折半查找 （有序表查找）
+function S_binarySearch(des, arr) {
+  if (!(arr instanceof Array))
+      return "arr is not array";
+  var low=0;
+  var high=arr.length;
+  var mid;
+  while (low<=high) {
+    mid=Math.floor((low+high)/2);
+    if(arr[mid]<des){
+      low=mid+1;
+    }else if (arr[mid]>des) {
+      high=mid-1;
+    }else{
+      return mid;
+    }
+  }
+  return 0;
+}
+
+//冒泡排序(改进)
+function S_bubbleSort(arr){
+  if (!(arr instanceof Array))
+      return "arr is not array";
+  var flag=true;
+  for(var i=0;i<arr.length-1 && flag;i++){
+    flag=false;
+    for(var j=arr.length-1;j>=i;j--){
+      if(arr[j]<arr[j-1]){
+        var temp=arr[j];
+        arr[j]=arr[j-1];
+        arr[j-1]=temp;
+        flag=true;
+      }
+    }
+  }
+  return arr;
+}
+
+//简单选择排序
+function S_selectSort(arr){
+  if (!(arr instanceof Array))
+      return "arr is not array";
+  for(var i=0;i<=arr.length-1;i++){  //这里是否加=
+    var min=i;
+    for(var j=i+1;j<=arr.length-1;j++){
+      if(arr[min]>arr[j])
+        min=j;
+    }
+    if(i!=min){
+      swap(arr,i,min)
+    }
+  }
+  return arr;
+}
+
+//快速排序
+function S_quickSort(arr){
+  if (!(arr instanceof Array))
+      return "arr is not array";
+  qsort(arr,0,arr.length-1);
+  return arr;
+}
+
+function qsort(arr,low,high){
+      var pivot;
+      if(low<high){
+        pivot=Partition(arr,low,high)
+        qsort(arr,low,pivot-1);
+        qsort(arr,pivot+1,high)
+      }
+}
+
+function Partition(arr,low,high){
+  var pivotkey=arr[low];
+  while(low<high){
+    while(low<high && arr[high]>=pivotkey)
+      high--;
+      swap(arr,low,high)
+    while(low<high && arr[low]<=pivotkey)
+      low++;
+      swap(arr,low,high)
+  }
+  return low;
+}
+
+//数字交换通用函数
+function swap(arr,a,b){
+  var temp = arr[a];
+  arr[a]=arr[b];
+  arr[b]=temp;
+  return arr;
+}
+
+var res = S_selectSort([2,6,3,7,1])
+console.log(res)
