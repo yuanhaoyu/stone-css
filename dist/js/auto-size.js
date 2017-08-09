d7b8d6ebbfedf33b86888e1a79a5e8856edb6513
Houn <yhy929768691@gmail.com>
2017-08-09 22:49:51 +0800

    fixed auto-size wechat bug

--- dist/js/auto-size.js
+++ dist/js/auto-size.js
@@ -2,8 +2,10 @@
      var docEle = doc.documentElement,
         evt = "onorientationchange" in window ? "orientationchange" : "resize",
         fn = function() {
-            var width = docEle.clientWidth;
-            width && (docEle.style.fontSize = 40 * (width / 750) + "px");
+            setTimeout(function(){
+                var width = docEle.clientWidth;
+                width && (docEle.style.fontSize = 40 * (width / 750) + "px");
+            },170)
         };//          html的字体大小  =  屏幕大小 / 设计图的大小
     win.addEventListener(evt, fn, false);
     doc.addEventListener("DOMContentLoaded", fn, false);
